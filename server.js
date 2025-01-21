ec026cffc67707fac9e3c4a791ddc5bb24b11b92
emi1y4 <190270476+emi1y4@users.noreply.github.com>
2025-01-21 00:22:27 +0000

    Jan 21, 2025, 1:22 AM

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 #!/usr/bin/env -S deno serve --allow-read=. --watch
 import { marked } from "npm:marked"
+import { serveDir } from "jsr:@std/http"
 
 export default {
-  async fetch(request, env) {
+  async fetch(request) {
     const url = new URL(request.url)
     let path = url.pathname.match(/^\/([^/]+\.html)?$/)
     if (!path) {
-      return new Response(null, { status: 404 })
+      return serveDir(request, {
+        fsRoot: ".",
+      })
     }
     const filename = (path[1] || "README") + ".md"
     const content = await Deno.readTextFile("./" + filename)
     const layout = await Deno.readTextFile("./_layouts/default.html")
     const vars = {
       content: marked(content),
-      title: content.match(/^# (.+)$/m)?.[1],
+      "page.title": content.match(/^# (.+)$/m)?.[1],
     }
     const page = layout.replace(/{{(.+?)}}/g, (_, key) => vars[key] || "")
     return new Response(page, {
