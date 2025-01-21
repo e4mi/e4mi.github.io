0fbec35430e7846b0d4f36ce00a6d0f6df5c50c6
emi1y4 <190270476+emi1y4@users.noreply.github.com>
2025-01-21 00:42:52 +0000

    Jan 21, 2025, 1:42 AM

--- server.js
+++ server.js
@@ -11,22 +11,23 @@ export default {
         fsRoot: ".",
       })
     }
+    const filename = (path[1] || "README") + ".md"
+    let content
     try {
-      const filename = (path[1] || "README") + ".md"
-      const content = await Deno.readTextFile("./" + filename)
-      const layout = await Deno.readTextFile("./_layouts/default.html")
-      const vars = {
-        content: marked(content).replace(/\.md">/g, '.html">'),
-        "page.title": content.match(/^# (.+)$/m)?.[1],
-      }
-      const page = layout.replace(/{{(.+?)}}/g, (_, key) => vars[key] || "")
-      return new Response(page, {
-        headers: {
-          "content-type": "text/html",
-        },
-      })
+      content = await Deno.readTextFile("./" + filename)
     } catch (error) {
       return new Response(null, { status: 404 })
     }
+    const layout = await Deno.readTextFile("./_layouts/default.html")
+    const vars = {
+      content: marked(content).replace(/\.md">/g, '.html">'),
+      "page.title": content.match(/^# (.+)$/m)?.[1],
+    }
+    const page = layout.replace(/{{(.+?)}}/g, (_, key) => vars[key] || "")
+    return new Response(page, {
+      headers: {
+        "content-type": "text/html",
+      },
+    })
   },
 }
